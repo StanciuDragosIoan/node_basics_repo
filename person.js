6afe9bdd08d26e7e629cf15034102fb970bf853a
Stanciu Dragos Ioan <stanciudragosioan92@gmail.com>
2020-01-09 01:53:21 +0200

    test

---
+++ person.js
@@ -0,0 +1,21 @@
+//module wrapper function
+// (function(exports, require, module, __filename, __dirname) { 
+//     // Module code actually lives in here 
+// }); 
+
+
+console.log(__dirname, __filename);
+
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting(){
+        console.log(`My name is ${this.name} and I am ${this.age}`);
+    }
+}
+
+module.exports = Person;
